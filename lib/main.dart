0fafbd6fbf32833cd9cb54bcdc02716e1a76da85
r.nanda.v <144448523+nandavernanda@users.noreply.github.com>
2024-04-23 23:04:42 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,125 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First App',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 72, 255, 0),
-          title: Text('Welcome to Flutter'),
+      title: 'Profile',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile Biodata'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ProfileCard(),
         ),
-        backgroundColor: Color.fromARGB(255, 0, 247, 255),
-        body: Center(
-          child: Text('RICKY AGUNG VERNANDA (STI202102168)'),
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 10,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.grey[200]?.withOpacity(0.3),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 20),
+                CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('assets/me.jpg'),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Ricky Agung Vernanda',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'STI202102168',
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                ),
+                SizedBox(height: 20),
+                ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text(
+                    'rickyagungvernanda@gmail.com',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.location_on),
+                  title: Text(
+                    'Jakarta, Indonesia',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.cake),
+                  title: Text(
+                    '20 Mei 2002',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.transgender),
+                  title: Text(
+                    'Laki-laki',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.star),
+                  title: Text(
+                    'Islam',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
